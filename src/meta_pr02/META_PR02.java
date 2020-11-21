05f168f1e2ff00818e3f438751bac2e5eff8178b
Sergio Perea de la Casa <spc00033@red.ujaen.es>
2020-11-21 17:29:59 +0100

    first commit

---
+++ src/meta_pr02/META_PR02.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package meta_pr02;
+
+/**
+ *
+ * @author spdlc
+ */
+public class META_PR02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
